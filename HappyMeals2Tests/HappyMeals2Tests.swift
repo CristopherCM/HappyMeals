ed991decc466320b7e820ad16f3fc4d07910e17d
CristopherCM <cristopher.cm99@hotmail.com>
2018-04-19 13:08:38 -0500

    Initial Commit

---
+++ HappyMeals2Tests/HappyMeals2Tests.swift
@@ -0,0 +1,36 @@
+//
+//  HappyMeals2Tests.swift
+//  HappyMeals2Tests
+//
+//  Created by Cristopher Alan Cejudo Machuca on 4/19/18.
+//  Copyright © 2018 Cristopher Alan Cejudo Machuca. All rights reserved.
+//
+
+import XCTest
+@testable import HappyMeals2
+
+class HappyMeals2Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
