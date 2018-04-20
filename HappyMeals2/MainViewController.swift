e8a23ea6f434f19142bf410cbe59e4bc0d0d07f6
CristopherCM <cristopher.cm99@hotmail.com>
2018-04-20 07:07:31 -0500

    first commit

---
+++ HappyMeals2/MainViewController.swift
@@ -0,0 +1,35 @@
+//
+//  MainViewController.swift
+//  HappyMeals2
+//
+//  Created by Cristopher Alan Cejudo Machuca on 4/19/18.
+//  Copyright © 2018 Cristopher Alan Cejudo Machuca. All rights reserved.
+//
+
+import UIKit
+
+class MainViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
